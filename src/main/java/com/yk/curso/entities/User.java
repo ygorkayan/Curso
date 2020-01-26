6623092f98b034136afd5b0f2ec3e7a454704768
Ygor Kayan <ygorkayan@gmail.com>
2020-01-26 09:02:49 -0300

    Colocando dependencias

--- src/main/java/com/yk/curso/entities/User.java
+++ src/main/java/com/yk/curso/entities/User.java
@@ -2,6 +2,9 @@ package com.yk.curso.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+
+@Entity
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
