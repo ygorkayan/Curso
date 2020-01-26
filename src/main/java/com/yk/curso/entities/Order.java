956159042bee35ef1e84e30387fce5ab923e0803
Ygor Kayan <ygorkayan@gmail.com>
2020-01-26 10:51:43 -0300

    Formatando a data

--- src/main/java/com/yk/curso/entities/Order.java
+++ src/main/java/com/yk/curso/entities/Order.java
@@ -11,6 +11,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 
@@ -23,6 +24,8 @@ public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z' ", timezone = "GMT")
     private Instant moment;
 
     @JsonIgnore
