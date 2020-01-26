18310acb112cd093241272eb0829d4295d4a815b
Ygor Kayan <ygorkayan@gmail.com>
2020-01-26 08:52:57 -0300

    Colocando o user

---
+++ src/main/java/com/yk/curso/resources/UserResource.java
@@ -0,0 +1,24 @@
+package com.yk.curso.resources;
+
+import com.yk.curso.entities.User;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * UserResource
+ */
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1, "ygor", "ygorkayan...", "123", "abc123");
+        return ResponseEntity.ok().body(u);
+    } 
+    
+}
